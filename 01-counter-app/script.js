4761ebc78be55b88d0cd3f4c28a952f5bf8d98f2
Rustam Ermatov <ermatovrus22@gmail.com>
2024-02-22 23:40:44 -0800

    counter app created and initial code is pushed

---
+++ 01-counter-app/script.js
@@ -0,0 +1,11 @@
+document.querySelector('.subtract').addEventListener('click', () => {
+  document.querySelector('.count').innerHTML--
+})
+
+document.querySelector('.add').addEventListener('click', () => {
+  document.querySelector('.count').innerHTML++
+})
+
+document.querySelector('.reset').addEventListener('click', () => {
+  document.querySelector('.count').innerHTML = 0
+})
